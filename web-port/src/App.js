e5a5f7e4adbb822286d493a133311db442d66ea8
CougarChan <gohan0136@gmail.com>
2022-11-25 15:04:11 -0500

    Hello World got most of the stuff labeled and we are getting ready to start this

---
+++ web-port/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Nav from "./Components/Navbar";
+
+function App() {
+   return (
+      <div>
+        <Nav></Nav>
+
+
+      </div>
+ 
+
+  )
+
+    
+  
+}
+
+export default App;
---
+++ web-port/src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Nav(){
+   return (
+        <h2 className="text-3xl font-bold underline">
+            Hello 
+        </h2>
+
+
+   )
+}
+
+
+export default Nav
