c4674228383dbaf7ea63122d52e47d4e12947b3e
CougarChan <gohan0136@gmail.com>
2022-12-04 16:51:38 -0500

    Almost finshed with nav

--- web-port/src/App.js
+++ web-port/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Nav from "./Components/Navbar";
 
+
 function App() {
    return (
       <div>
--- web-port/src/Components/Navbar.jsx
+++ web-port/src/Components/Navbar.jsx
@@ -1,12 +1,37 @@
 import React from "react";
+import { GoGrabber } from "react-icons/go";
+import { useState } from "react";
 
-function Nav(){
+
+
+const Nav = () => {
+   const  [set, setTime] =useState(false)
+   const handleClick = () => setTime(!set)
    return (
-        <h2 className="text-3xl font-bold underline">
-            Hello 
+    <div className="fixed w-full bg-[#000] flex justify-between items-center px-5 text-gray-50 h-24" >
+         <h2 className="text-3xl font-bold underline"> German Lopez 
         </h2>
+        
+           <ul className="hidden md:flex">
+              <li>About Me</li>
+              <li>Projects</li>
+              <li>Contact Me!</li>
+           </ul>
+    
+          <div onClick={handleClick} className="md-hidden z-10 cursor-pointer">
+            <GoGrabber />
+          </div>
 
+            <ul className={!set ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-black flex flex-col justify-center items-center '}>
+              <li className="py-6 text-3xl">About Me</li>
+              <li className="py-6 text-3xl">Projects</li>
+              <li className="py-6 text-3xl">Contact Me!</li>
+           </ul>
+    
 
+        
+         
+    </div>
    )
 }
 
